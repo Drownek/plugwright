2b5497895b5e7d4af0ec27beace8fcc547593c8f
Drownek <piotrgamingyt@gmail.com>
2026-06-22 15:24:32 +0200

    chore: bump to 1.3.3

--- example_plugin/build.gradle.kts
+++ example_plugin/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     `java-library`
     id("de.eldoria.plugin-yml.bukkit") version "0.8.0"
     id("com.gradleup.shadow") version "9.0.0"
-    id("io.github.drownek.paperwright") version "1.3.3-dev.0"
+    id("io.github.drownek.paperwright") version "1.3.3"
 }
 
 paperwright {
